c1015e25c1a5f473df77c2727a05146c44aa6ca2
Mats Lindblad <mats.lindblad@gmail.com>
2023-07-21 14:32:44 +0200

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { Store } from '@ngrx/store';
+import { FavoritesState } from './favorites-state';
+import * as FavoriteActions from './store/actions';
+import { FavoritesSheetComponent } from './favorites-sheet/favorites-sheet.component';
+import { selectAllFavorites, selectFavorites } from './store/selectors';
+import { Observable } from 'rxjs';
+import { Cocktail } from './cocktail';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cocktails';
+  favorites$!: Observable<Cocktail[]>;
+  showFavoriteSheetIcon: boolean = false;
+
+  constructor(
+    private _bottomSheet: MatBottomSheet,
+    private store: Store<any>
+  ) {}
+
+  ngOnInit() {
+    this.store.dispatch(FavoriteActions.LoadFavorites());
+    this.favorites$ = this.store.select(selectAllFavorites);
+    this.favorites$.subscribe((favorites) => {
+      this.showFavoriteSheetIcon = favorites.length > 0;
+    });
+  }
+
+  openFavoritesSheet() {
+    this._bottomSheet.open(FavoritesSheetComponent);
+  }
 }
